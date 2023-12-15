8ea4c2d3fcdb7d0135483d43c549cafddda7ad2f
erikvw <ew2789@gmail.com>
2023-12-14 20:33:40 -0600

    test setup

--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
+|pypi| |actions| |codecov| |downloads|
+
+
 edc-lab-panel
 -------------
 
@@ -11,3 +14,15 @@ A collection of common lab panels used in edc deployments
 * Glucose
 
 Also included are normal and graded ranges for the test results in these panels.
+
+.. |pypi| image:: https://img.shields.io/pypi/v/edc-lab-panel.svg
+    :target: https://pypi.python.org/pypi/edc-lab-panel
+
+.. |actions| image:: https://github.com/clinicedc/edc-lab-panel/actions/workflows/build.yml/badge.svg
+  :target: https://github.com/clinicedc/edc-lab-panel/actions/workflows/build.yml
+
+.. |codecov| image:: https://codecov.io/gh/clinicedc/edc-lab-panel/branch/develop/graph/badge.svg
+  :target: https://codecov.io/gh/clinicedc/edc-lab-panel
+
+.. |downloads| image:: https://pepy.tech/badge/edc-lab-panel
+   :target: https://pepy.tech/project/edc-lab-panel
--- runtests.py
+++ runtests.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 import logging
-import sys
-from os.path import abspath, dirname, join
+from pathlib import Path
 
-import django
-from django.conf import settings
-from django.test.runner import DiscoverRunner
-from edc_test_utils import DefaultTestSettings
+from edc_test_utils import DefaultTestSettings, func_main
 
-base_dir = dirname(abspath(__file__))
 app_name = "edc_lab_panel"
 
-DEFAULT_SETTINGS = DefaultTestSettings(
+base_dir = Path(__file__).absolute().parent
+
+project_settings = DefaultTestSettings(
     calling_file=__file__,
     BASE_DIR=base_dir,
     APP_NAME=app_name,
-    ETC_DIR=join(base_dir, app_name, "tests", "etc"),
     SUBJECT_SCREENING_MODEL="edc_metadata.subjectscreening",
     SUBJECT_CONSENT_MODEL="edc_metadata.subjectconsent",
     SUBJECT_VISIT_MODEL="edc_visit_tracking.subjectvisit",
@@ -35,34 +31,23 @@ DEFAULT_SETTINGS = DefaultTestSettings(
         "edc_appointment.apps.AppConfig",
         "edc_action_item.apps.AppConfig",
         "edc_crf.apps.AppConfig",
-        # "edc_device.apps.AppConfig",
-        # "edc_dashboard.apps.AppConfig",
-        # "edc_facility.apps.AppConfig",
         "edc_lab.apps.AppConfig",
         "edc_metadata.apps.AppConfig",
-        # "edc_offstudy.apps.AppConfig",
         "edc_registration.apps.AppConfig",
         "edc_identifier.apps.AppConfig",
         "edc_notification.apps.AppConfig",
         "edc_sites.apps.AppConfig",
-        # "edc_timepoint.apps.AppConfig",
         "edc_visit_schedule.apps.AppConfig",
         "edc_visit_tracking.apps.AppConfig",
         "edc_lab_results.apps.AppConfig",
     ],
-    RANDOMIZATION_LIST_PATH=join(base_dir, app_name, "tests", "test_randomization_list.csv"),
     add_dashboard_middleware=True,
     use_test_urls=True,
 ).settings
 
 
 def main():
-    if not settings.configured:
-        settings.configure(**DEFAULT_SETTINGS)
-    django.setup()
-    tags = [t.split("=")[1] for t in sys.argv if t.startswith("--tag")]
-    failures = DiscoverRunner(failfast=False, tags=tags).run_tests([f"{app_name}.tests"])
-    sys.exit(failures)
+    func_main(project_settings, f"{app_name}.tests")
 
 
 if __name__ == "__main__":
